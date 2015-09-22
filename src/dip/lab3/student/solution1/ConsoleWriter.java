88c51fa370a2b0d7fb60fce4ce8a46d66069e3d8
tduwe <tduwe@L11710042773.wctc.msft>
2015-09-22 15:24:13 -0500

    Added GUI interface to ConsoleWriter Class so it outputs in both console and GUI

--- src/dip/lab3/student/solution1/ConsoleWriter.java
+++ src/dip/lab3/student/solution1/ConsoleWriter.java
@@ -1,12 +1,16 @@
 
 package dip.lab3.student.solution1;
 
+import javax.swing.JOptionPane;
+
 
 public class ConsoleWriter implements Writer {
 	
 	
+        @Override
 	public void writeln( String line ) {
             System.out.println("Here is the stuff you entered");
-            System.out.println( line );
+            System.out.println(line);
+            JOptionPane.showMessageDialog(null, line);
 	}
 }
