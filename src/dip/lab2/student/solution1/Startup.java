849e43611a9b3eac48d8d196be0c22e5e2b583a9
Thomas <Thomas@10.30.18.253>
2015-09-22 12:05:20 -0500

    added code to startup class

--- src/dip/lab2/student/solution1/Startup.java
+++ src/dip/lab2/student/solution1/Startup.java
@@ -2,8 +2,7 @@ package dip.lab2.student.solution1;
 
 // An useful import if you need it.
 import java.text.NumberFormat;
-// Another useful import if you need it.
-import javax.swing.JOptionPane;
+
 
 /*
  * 
@@ -13,7 +12,9 @@ public class Startup {
      
     public static void main(String[] args) {
         
-        
+        Service tipService = new Service(new BaggageServiceTipCalculator(ServiceQuality.GOOD, 5));
+        NumberFormat nf = NumberFormat.getCurrencyInstance();
+        System.out.println(nf.format(tipService.getTip()));
         
     }
 
